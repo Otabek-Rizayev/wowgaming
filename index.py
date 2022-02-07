feed7c59020bf1838b115d18b5b8ec474bd382eb
Otabek Rizayev <46789195+Otabek-Rizayev@users.noreply.github.com>
2022-02-07 19:15:22 +0500

    My first commit

--- index.py
+++ index.py
@@ -2,8 +2,11 @@ import requests
 from bs4 import BeautifulSoup
 
 def get_online():
-    url = "http://wowgaming.uz/"
-    page = requests.get(url)
-    soup = BeautifulSoup(page.content, "html.parser")
-    price = soup.find(class_="col-lg-12 text-center").get_text()[45:][:19]
-    return f"♻️ {price}"
+    try:
+        url = "http://wowgaming.uz/"
+        page = requests.get(url)
+        soup = BeautifulSoup(page.content, "html.parser")
+        price = soup.find(class_="col-lg-12 text-center").get_text()[45:][:19]
+        return f"♻️ {price}"
+    except:
+        return("server vaqtincha ishlamayapti...")
