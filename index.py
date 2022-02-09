a5518826fc97668035f8e0eabe193ac97453bc37
Otabek Rizayev <46789195+Otabek-Rizayev@users.noreply.github.com>
2022-02-09 12:13:23 +0500

    bug fixed

--- index.py
+++ index.py
@@ -6,8 +6,10 @@ def get_online():
         url = "http://wowgaming.uz/"
         page = requests.get(url)
         soup = BeautifulSoup(page.content, "html.parser")
-        price = soup.find(class_="col-lg-12 text-center").get_text()[45:][:18]
-        return f"♻️ {price}"
+        price = soup.find(class_="col-lg-12 text-center").get_text()[45:][:19]
+        a = f"♻ {price}"
+        print(a.replace(")", " "))
     except:
         return "server vaqtincha ishlamayapti..."
 
+print(get_online())
