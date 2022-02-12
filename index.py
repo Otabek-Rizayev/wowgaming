adde58a7bd6cb1cfb59427f052d37f28162d1e4b
Otabek Rizayev <46789195+Otabek-Rizayev@users.noreply.github.com>
2022-02-12 12:15:51 +0500

    Update index.py

--- index.py
+++ index.py
@@ -18,7 +18,7 @@ def get_who():
         price2 = soup.find(class_="table table-striped").get_text()[20:]   
         b = list(price2)
         c = "".join(b)
-        d = b.replace("80", "\n")
+        d = c.replace("80", "\n")
         return f"🌐 Hozir o`yinda: \n{c}"
 
     
