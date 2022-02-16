e9d34e18db991980b82f1d8526d9d0f7e7c2a752
Otabek Rizayev <46789195+Otabek-Rizayev@users.noreply.github.com>
2022-02-16 23:24:44 +0500

    fix

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 import logging
 from aiogram import Bot, Dispatcher, executor, types
-from index import get_online
-from index import get_who
-API_TOKEN = '5280417676:AAEKx7yiw6L10Cv74qgRETk53Kpip9Vpg9E'
+#from index import get_online, get_who
+from bs4 import BeautifulSoup
+import asyncio
+import aiohttp
+from aiohttp_requests import requests
+
+page = requests.get(url)
+soup = BeautifulSoup(page.content, "html.parser")
+
+API_TOKEN = '5146056686:AAFsI3qbDMwGTSdckEiObbBhgIz2e0Npphg'
 # Configure logging
 logging.basicConfig(level=logging.INFO)
 
@@ -10,22 +17,35 @@ logging.basicConfig(level=logging.INFO)
 bot = Bot(token=API_TOKEN)
 dp = Dispatcher(bot)
 
-@dp.message_handler(commands=['start'])
-async def send_welcome(message: types.Message):
-    
-    await message.reply("WoWGaming botga hush kelibsiz.\nYordam uchun /help buyrug'ini kiriting!")
+async def main():
+    async with aiohttp.ClientSession() as session:
+        async with session.get('http://wowgaming.uz') as response:
+            text = await response.text(encoding="unicode_escape")
+            soup = BeautifulSoup(text, "html.parser")
+            #price = soup.find(class_="col-lg-12 text-center").get_text()[45:][:19]
+            #return price
 
-@dp.message_handler(commands=['help'])
-async def help_cmd(message: types.Message):
-    await message.reply("Yordam!\n- /online buyrug'i orqali serverda qancha online borligini ko'rish mumkin.\n- /who buyrug'i orqali serverdagi barcha playerlar ro`yxatini ko'rish mumkin.")
+asyncio.run(main())
 
 @dp.message_handler(commands=['online'])
 async def online_cmd(message: types.Message):
-    await message.answer(get_online())
+    try:
+        price = soup.find(class_="col-lg-12 text-center").get_text()[45:][:19]
+        a = f"♻ {price}"
+        b = a.replace(")", "  ")
+        await message.answer(b)
+    except:
+        await message.reply("server vaqtincha ishlamayapti...")
+
 
 @dp.message_handler(commands=['who'])
 async def who_cmd(message: types.Message):
-    await message.answer(get_who())
+    price2 = soup.find(class_="table table-striped").get_text()[20:]   
+    c = list(price2)
+    d = "".join(c)
+    e = d.replace("80", "\n")
+    f = f"🌐 Hozir o`yinda: \n{e}"
+    await message.answer(f)
 
 
 if __name__ == '__main__':
