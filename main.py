e7cb0e5d0c07fee6768ddecaf819d70b51865521
Otabek Rizayev <46789195+Otabek-Rizayev@users.noreply.github.com>
2022-02-18 18:22:11 +0500

    fix

--- main.py
+++ main.py
@@ -4,10 +4,6 @@ from aiogram import Bot, Dispatcher, executor, types
 from bs4 import BeautifulSoup
 import asyncio
 import aiohttp
-from aiohttp_requests import requests
-
-page = requests.get(url)
-soup = BeautifulSoup(page.content, "html.parser")
 
 API_TOKEN = '5146056686:AAFsI3qbDMwGTSdckEiObbBhgIz2e0Npphg'
 # Configure logging
@@ -17,35 +13,33 @@ logging.basicConfig(level=logging.INFO)
 bot = Bot(token=API_TOKEN)
 dp = Dispatcher(bot)
 
-async def main():
-    async with aiohttp.ClientSession() as session:
-        async with session.get('http://wowgaming.uz') as response:
-            text = await response.text(encoding="unicode_escape")
-            soup = BeautifulSoup(text, "html.parser")
-            #price = soup.find(class_="col-lg-12 text-center").get_text()[45:][:19]
-            #return price
-
-asyncio.run(main())
-
+#asyncio.run(main())
 @dp.message_handler(commands=['online'])
 async def online_cmd(message: types.Message):
-    try:
-        price = soup.find(class_="col-lg-12 text-center").get_text()[45:][:19]
-        a = f"♻ {price}"
-        b = a.replace(")", "  ")
-        await message.answer(b)
-    except:
-        await message.reply("server vaqtincha ishlamayapti...")
+    async with aiohttp.ClientSession() as session:
+        async with session.get('http://wowgaming.uz') as response:
+            text = await response.text(encoding="utf-8", errors="ignore")
+            soup = BeautifulSoup(text, "html.parser")
+            price = soup.find(class_="col-lg-12 text-center").get_text()[45:][:19]
+            a = f"♻ {price}"
+            b = a.replace(")", "  ")
+            await message.answer(b)
+#except:
+        #await message.reply("server vaqtincha ishlamayapti...")
 
 
 @dp.message_handler(commands=['who'])
 async def who_cmd(message: types.Message):
-    price2 = soup.find(class_="table table-striped").get_text()[20:]   
-    c = list(price2)
-    d = "".join(c)
-    e = d.replace("80", "\n")
-    f = f"🌐 Hozir o`yinda: \n{e}"
-    await message.answer(f)
+    async with aiohttp.ClientSession() as session:
+        async with session.get('http://wowgaming.uz') as response:
+            text = await response.text(encoding="utf-8", errors="ignore")
+            soup = BeautifulSoup(text, "html.parser")
+            price2 = soup.find(class_="table table-striped").get_text()[20:]   
+            c = list(price2)
+            d = "".join(c)
+            e = d.replace("80", "\n")
+            f = f"🌐 Hozir o`yinda: \n{e}"
+            await message.answer(f)
 
 
 if __name__ == '__main__':
